83ff9689b4d5bc05295fdfe7484aa2937081881f
Riao Mike <xuruimaths@qq.com>
2022-11-01 22:54:13 +0800

    nov 1 evening

--- Chapter8/8_6.cpp
+++ Chapter8/8_6.cpp
@@ -11,9 +11,38 @@ struct free_throws {
 
 void display(const free_throws & ft);
 void set_pc(free_throws & ft);
-free_throws & accumulates(free_throws & target, const free_throws & source);
+free_throws & accumulate(free_throws & target, const free_throws & source);
 
 int main() {
+    // partial initializations - remaining members set to 0
+    free_throws one = {"Ifelsa Branch", 13, 14};
+    free_throws two = {"Andor Knott", 10, 16};
+    free_throws three = {"Minnie Max", 7, 9};
+    free_throws four = {"Whily Looper", 5, 9};
+    free_throws five = {"Long Long", 6, 14};
+    free_throws team = {"Throwgoos", 0, 0};
+    // no initialization
+    free_throws dup;
+
+    set_pc(one);
+    display(one);
+    accumulate(team, one);
+    display(team);
+    // use return value as argument
+    display(accumulate(team, two));
+    accumulate(accumulate(team, three), four);
+    display(team);
+    // use return value in assignment
+    dup = accumulate(team, five);
+    cout << "Displaying team" << endl;
+    display(team);
+    cout << "Displaying dup after assignment" << endl;
+    display(dup);
+    set_pc(four);
+    // ill-advised assignment
+    accumulate(dup, five) = four;
+    cout << "Displaying dup after ill-advised assignment:" << endl;
+    display(dup);
     return 0;
 }
 
@@ -29,4 +58,11 @@ void set_pc(free_throws & ft) {
         ft.percent = 100.0f * ft.made / ft.attempts;
     else
         ft.percent = 0;
+}
+
+free_throws & accumulate(free_throws & target, const free_throws & source) {
+    target.attempts += source.attempts;
+    target.made += source.made;
+    set_pc(target);
+    return target; 
 }
